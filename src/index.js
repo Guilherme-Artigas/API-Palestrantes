53590a72a781a45bee67ee9a88b5d57d08beded2
Guilherme Artigas <Guilherme.Artigas92@gmail.com>
2022-12-14 15:45:53 -0300

    docs: Startando o Projeto!🎙

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-// não remova esse endpoint, e para o avaliador funcionar
+// Endpoint essencial para o avaliador funcionar! Não remove-lo!!!
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
